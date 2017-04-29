ea4f104340c69ad12b494ed974c28e427ec7cbfa
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-04-28 17:01:08 -0700

    lesson3

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.use(bodyParser.urlencoded({
 
 let fruits = [];
 
+const findById = (id) => {
+	for(let i = 0; i < fruits.length; i+=1){
+		if(fruits[i].id === id){
+			return i;
+		}
+	}
+}
+
 const rootHandler = (req, res, next) => {
 	res.status(200).json({message: 'Welcome to Node API'});
 }
@@ -18,17 +26,37 @@ const get = (req, res, next) => {
 	res.status(200).json(fruits);
 };
 const create = (req, res, next) => {
-	fruits.push(req.body);
+	let fruit = req.body;
+	fruit.id = Math.floor(Math.random() * 50000);
+	fruits.push(fruit);
 	res.status(201).json(fruits);
 };
-const update = (req, res, next) => {};
-const remove = (req, res, next) => {};
+const update = (req, res, next) => {
+	const index = findById(req.body.id);
+	fruits[index].name = req.body.name;
+	res.status(201).json(fruits);
+};
+const remove = (req, res, next) => {
+	const index = findById(req.body.id);
+	fruits.splice(index, 1);
+	res.status(200).json(fruits);
+};
+
+const findOne = (req, res, next) => {
+	const index = findById(parseInt(req.params.id));
+	res.status(201).json(fruits[index]);
+};
+
 
 app.get('/fruits', get);
 app.post('/fruits', create);
 app.put('/fruits', update);
 app.delete('/fruits', remove);
 
+// Find by ID
+app.get('/fruits/:id', findOne);
+
+//Root Handler
 app.get('/', rootHandler);
 
 module.exports = app;
