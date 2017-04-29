940c47aaa6ae8d78c587b6e58f9e62b964d073e9
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-04-28 22:26:57 -0700

    lesson6

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const debug = require('debug')('app');
 
 require('./config/middleware.express')(app);
+debug('Application middleware configured');
+
 require('./config/routes')(app);
+debug('Application routes loaded');
 
 module.exports = app; 
+debug('Application exported using module.exports');
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const debug = require('debug')('app');
 
 app.listen(8080, () =>{
-	console.log("express server running on port 8080");
+	debug('Express server running on port 8080');
 });
