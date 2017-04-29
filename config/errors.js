622f95274441535e222c114e1b656b713e2579b1
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-04-28 22:57:21 -0700

    lesson8

---
+++ config/errors.js
@@ -0,0 +1,5 @@
+module.exports = (err, req, res, next) => {
+	if(err) {
+		res.status(500).json(error);
+	}
+}
--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
 const fruitRouter = require('../api/fruit/fruit.router');
 const rootRouter = require('../api/root/root.router');
+const errors = require('./errors');
 
 module.exports = (app) =>{
 	app.use('/', rootRouter);
 	app.use('/fruits', fruitRouter);
+	app.use(errors);
 }
